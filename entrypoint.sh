c49d45b3a08ccbed186d0e1fd4095cc1d0859e51
Andre van Schalkwyk <andre@hebe.ch>
2020-12-01 13:26:23 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
+echo ".kanbanviewrc specifies the following DB file: $(grep thingsdb /root/.kanbanviewrc | awk '{print $3}')"
 if [ -z "$THINGSDB" ]
 then 
   echo "No variable defined for Things sqlite DB"
   export THINGSDB=$(ls /things/*.sqlite*)
   echo "Using DB file: $THINGSDB"
+  echo "If this is incorrect, modify ENV THINGSDB accordingly."
 else
   echo "Starting with DB file: $THINGSDB"
 fi
 #sed -i "/thingsdb/c\thingsdb = $THINGSDB" /root/.kanbanviewrc
-echo ".kanbanviewrc - DB file location: $(grep thingsdb /root/.kanbanviewrc)"
+
 cd /usr/src/KanbanView/ && make run-api
