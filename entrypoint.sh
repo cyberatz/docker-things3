19cc464396227ca8ff63aa8c0fec8f8741c99e3c
Andre van Schalkwyk <andre@hebe.ch>
2020-12-01 13:53:48 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+sed -i "/background-image: linear-gradient(45deg, rgb(100, 50, 50), rgb(150, 50, 50));/c\background-image: linear-gradient(45deg, rgb(100, 100, 100), rgb(20, 20, 155));" /usr/src/KanbanView/resources/kanban.css
+sed -i "/background: #fff;/c\background: #d4e5fc;" /usr/src/KanbanView/resources/kanban.css
+
+curl -s -o /usr/src/KanbanView/resources/logo-dark.png https://culturedcode.com/things/2017-03-25/images/appicon-mac.png
+
 echo ".kanbanviewrc specifies the following DB file: $(grep thingsdb /root/.kanbanviewrc | awk '{print $3}')"
 if [ -z "$THINGSDB" ]
 then 
