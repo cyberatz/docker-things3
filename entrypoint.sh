8856eed7b572458267068775f95cbbddbf0c22de
Andre van Schalkwyk <andre@hebe.ch>
2020-12-01 12:42:45 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
+if [ -z "$THINGSDB" ]
+then 
+  echo "No variable defined for Things sqlite DB"
+  THINGSDB=$(ls /things/*.sqlite*)
+  echo "Using DB file: $THINGSDB"
+else
+  echo "Starting with DB file: $THINGSDB"
+fi
+
 cd /usr/src/KanbanView/ && make run-api
