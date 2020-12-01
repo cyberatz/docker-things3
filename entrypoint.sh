91d85d5fd58ba01b5efc9348ac3406e733e3bfc7
Andre van Schalkwyk <andre@hebe.ch>
2020-12-01 14:19:45 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-sed -i "/background-image: linear-gradient(45deg, rgb(100, 50, 50), rgb(150, 50, 50));/c\background-image: linear-gradient(45deg, rgb(100, 100, 100), rgb(20, 20, 155));" /usr/src/KanbanView/resources/kanban.css
-sed -i "/background: #fff;/c\background: #d4e5fc;" /usr/src/KanbanView/resources/kanban.css
+#sed -i "/background-image: linear-gradient(45deg, rgb(100, 50, 50), rgb(150, 50, 50));/c\background-image: linear-gradient(45deg, rgb(100, 100, 100), rgb(20, 20, 155));" /usr/src/KanbanView/resources/kanban.css
+#sed -i "/background: #fff;/c\background: #d4e5fc;" /usr/src/KanbanView/resources/kanban.css
 
 curl -s -o /usr/src/KanbanView/resources/logo-dark.png https://culturedcode.com/things/2017-03-25/images/appicon-mac.png
 
