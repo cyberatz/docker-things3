237da0322d1aadc226a1a54bb45669e9273f8a6f
Andre van Schalkwyk <andre@hebe.ch>
2020-12-01 14:59:38 +0100

    Update kanban.js

--- kanban.js
+++ kanban.js
@@ -391,7 +391,7 @@ async function statsShowDistribution () { // eslint-disable-line no-unused-vars
             'rgba(54, 162, 235, 0.4)',
             'rgba(255, 206, 86, 0.4)',
             'rgba(75, 192, 192, 0.4)',
-            'rgba(153, 102, 255, 0.4)',
+            'rgba(15, 84, 163, 0.4)',
             'rgba(255, 159, 64, 0.4)'
           ],
           borderColor: [
@@ -399,7 +399,7 @@ async function statsShowDistribution () { // eslint-disable-line no-unused-vars
             'rgba(54, 162, 235, 1)',
             'rgba(255, 206, 86, 1)',
             'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
+            'rgba(15, 84, 163, 1)',
             'rgba(255, 159, 64, 1)'
           ],
           borderWidth: 1
